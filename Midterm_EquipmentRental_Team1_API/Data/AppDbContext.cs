a4260386bc80d1b95164edb89704a1d7f305c9d7
JonMcD9185 <Jmcdowall9185@conestogac.on.ca>
2025-10-20 21:04:24 -0400

    Removed DbSet<User>

--- Midterm_EquipmentRental_Team1_API/Data/AppDbContext.cs
+++ Midterm_EquipmentRental_Team1_API/Data/AppDbContext.cs
@@ -8,7 +8,6 @@ public class AppDbContext : DbContext
     public DbSet<Equipment> Equipment { get; set; }
     public DbSet<Customer> Customers { get; set; }
     public DbSet<Rental> Rentals { get; set; }
-    public DbSet<User> Users { get; set; }
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
