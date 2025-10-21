ca0140b0e44eec33c01fba014aea8e394168634f
GregGrondin <ronding36@gmail.com>
2025-10-20 23:03:15 -0400

    numba 3

--- Midterm_EquipmentRental_Team1_UI/Controllers/AuthController.cs
+++ Midterm_EquipmentRental_Team1_UI/Controllers/AuthController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Midterm_EquipmentRental_Team1_UI.Models;
+using System.Text.Json;
+using Midterm_EquipmentRental_Team1_Models;
 
 namespace Midterm_EquipmentRental_Team1_UI.Controllers
 {
@@ -9,6 +12,50 @@ namespace Midterm_EquipmentRental_Team1_UI.Controllers
         public AuthController(IHttpClientFactory httpClientFactory)
         {
             this.httpClientFactory = httpClientFactory;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var client = httpClientFactory.CreateClient();
+            var loginRequest = new LoginRequest
+            {
+                Username = model.Username,
+                Password = model.Password,
+            };
+
+            var response = await client.PostAsJsonAsync("https://localhost:7212/api/auth/login", loginRequest);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var token = await response.Content.ReadAsStringAsync();
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true,
+                };
+                var result = JsonSerializer.Deserialize<JsonElement>(token, options);
+
+                model.Token = result.GetProperty("token").GetString();
+                HttpContext.Session.SetString("JWToken", model.Token);
+                return RedirectToAction("Index", "Product");
+            }
+            else
+            {
+                model.ErrorMessage = "Invalid username or password";
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("JWToken");
+            return RedirectToAction("Login", "Auth");
         }
     }
 }
